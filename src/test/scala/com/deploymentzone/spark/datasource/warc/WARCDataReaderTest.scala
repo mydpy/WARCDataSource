252912ab231675234a7569236c3c148b4705cce1
Charles Feduke <charles.feduke@gmail.com>
2019-05-04 19:24:00 -0400

    updated unit test

--- src/test/scala/com/deploymentzone/spark/datasource/warc/WARCDataReaderTest.scala
+++ src/test/scala/com/deploymentzone/spark/datasource/warc/WARCDataReaderTest.scala
@@ -3,6 +3,7 @@ package com.deploymentzone.spark.datasource.warc
 import java.sql.Timestamp
 import java.time.LocalDateTime
 
+import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
 import org.scalatest._
 
@@ -13,7 +14,7 @@ class WARCDataReaderTest extends FlatSpec {
     val path = "/Users/charles.feduke/Downloads/CC-MAIN-20190418101243-20190418122315-00032.warc.gz"
     val reader = new WARCDataReader(path)
     val row = reader.get()
-    val gr = new GenericRowWithSchema(row.toSeq(WARCDataSource.schema).toArray, WARCDataSource.schema)
+    val gr = createGenericRow(row)
     assert(gr.getAs[String]("WARC-Type") === "warcinfo")
     assert(gr.getAs[Timestamp]("WARC-Date") ===
       java.sql.Timestamp.valueOf(LocalDateTime.parse("2019-04-18T10:12:43Z", WARCDataSource.dateTimeFormat)))
@@ -21,6 +22,18 @@ class WARCDataReaderTest extends FlatSpec {
     assert(gr.getAs[String]("WARC-Record-ID") === "<urn:uuid:445f0832-23e7-4d73-9c26-a2089f0edef9>")
     assert(gr.getAs[Int]("Content-Length") === 501)
 
+
+    debugGenericRow(gr)
+    debugGenericRow(createGenericRow(reader.get()))
+    debugGenericRow(createGenericRow(reader.get()))
+    debugGenericRow(createGenericRow(reader.get()))
+  }
+
+  private def createGenericRow(row: InternalRow): GenericRowWithSchema = {
+    new GenericRowWithSchema(row.toSeq(WARCDataSource.schema).toArray, WARCDataSource.schema)
+  }
+
+  private def debugGenericRow(gr: GenericRowWithSchema): Unit = {
     gr.schema.fieldNames.map { fieldName =>
       val idx = gr.schema.fieldIndex(fieldName)
       val value = gr.get(idx)
